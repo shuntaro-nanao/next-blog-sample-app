2dd30907a337239ca73216fb6e178fa8a84b5c99
shuntaro nanao <shuntaro.nno0319@gmail.com>
2021-01-23 13:20:39 +0900

    deploy error fix

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from './header'
+import Header from './Header'
 import layoutStyles from 'assets/scss/layout/layout.module.scss'
 
 export const siteTitle = 'Next.js Sample Website'
