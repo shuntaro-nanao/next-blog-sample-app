f00930fec768c5ed287b8cd2381f37f2af88c21f
shuntaro nanao <shuntaro.nno0319@gmail.com>
2021-03-01 19:16:40 +0900

    add Pagination css

--- components/Component/Pagination.tsx
+++ components/Component/Pagination.tsx
@@ -1,53 +1,30 @@
+import paginationStyles from '@/assets/scss/object/component/Pagination.module.scss'
 interface Props {
   perPage?: number;
 };
 
 const Pagination: React.FC<Props> = (perPage) => {
   return (
-    <div className="mt-10 pb-4 pt-4 flex items-center justify-between">
-      <div className="flex-1 flex justify-between sm:hidden">
-        <a href="#" className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-          Previous
-        </a>
-        <a href="#" className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-          Next
-        </a>
-      </div>
-      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-center">
-        <div>
-          <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-            <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-              <span className="sr-only">Previous</span>
-              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"/>
-              </svg>
-            </a>
-            <a href="#" className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              1
-            </a>
-            <a href="#" className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              2
-            </a>
-            <a href="#" className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              3
-            </a>
-            <a href="#" className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              8
-            </a>
-            <a href="#" className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              9
-            </a>
-            <a href="#" className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-              10
-            </a>
-            <a href="#" className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-              <span className="sr-only">Next</span>
-              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"/>
-              </svg>
-            </a>
-          </nav>
-        </div>
+    <div className={paginationStyles.c_pagination}>
+      <div className={paginationStyles.c_pagination__body}>
+        <nav className={paginationStyles.c_pagination__nav} aria-label="Pagination">
+          <a href="#" className={paginationStyles.c_pagination__prev}>
+            <svg className={paginationStyles.c_pagination__icon}>
+              <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"/>
+            </svg>
+          </a>
+          <a href="#" className={paginationStyles.c_pagination__number}>
+            1
+          </a>
+          <a href="#" className={paginationStyles.c_pagination__number}>
+            2
+          </a>
+          <a href="#" className={paginationStyles.c_pagination__next}>
+            <svg className={paginationStyles.c_pagination__icon}>
+              <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"/>
+            </svg>
+          </a>
+        </nav>
       </div>
     </div>
   )
